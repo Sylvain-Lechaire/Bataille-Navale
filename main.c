6ad7d3d94a911ad704105db807ae923f995ec701
LECHAIRE Sylvain <sylvain.LECHAIRE@cpnv.ch>
2020-03-05 15:51:53 +0100

    commit de verification avec Mr Favre

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int menu;
+printf("bataille navale \n");
+printf ("bonjour, bienvenue sur la bataille navale, choissiez une des options si dessous");
+printf("1 demarrer le jeu");
+printf("2 afficher l'aide");
+printf("3 sortir du programme");
+scanf("%d", &menu);
+if (menu>3)
+    printf("erreur chiffre trop grand");
+
+switch (menu)
+    {
+        case 1:
+            printf("demarrer le jeu");
+        break;
+    }
+
+
+
     return 0;
 }
