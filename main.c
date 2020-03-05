18531dfca4b682036d4567b43d24688be1993392
LECHAIRE Sylvain <Sylvain.LECHAIRE@cpnv.ch>
2020-03-05 16:21:52 +0100

    menu principal fonctionel et debut du code pour l'aide

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 int main() {
     int menu;
 printf("bataille navale \n");
-printf ("bonjour, bienvenue sur la bataille navale, choissiez une des options si dessous");
-printf("1 demarrer le jeu");
-printf("2 afficher l'aide");
-printf("3 sortir du programme");
-scanf("%d", &menu);
-if (menu>3)
-    printf("erreur chiffre trop grand");
+printf ("bonjour, bienvenue sur la bataille navale, choissiez une des options si dessous\n");
+do {
+    printf("1 demarrer le jeu\n");
+    printf("2 afficher l'aide\n");
+    printf("3 sortir du programme\n");
+    scanf("%d", &menu);
+    if (menu > 3)
+        printf("erreur chiffre trop grand\n");
 
-switch (menu)
-    {
+    switch (menu) {
         case 1:
-            printf("demarrer le jeu");
-        break;
+            printf("demarrer le jeu\n");
+            break;
+        case 2:
+            printf("aide\n");
+            char aide;
+            aide = "a";
+            printf("%c", aide);
+            break;
     }
 
-
+}while(menu>3);
 
     return 0;
 }
