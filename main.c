8a9b8ef20abd09aeb401bab8e2029acfcff1aec1
LECHAIRE Sylvain <Sylvain.LECHAIRE@cpnv.ch>
2020-03-13 14:55:40 +0100

    WIK coder le jeu

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
+/*/
+ * projet: bataille navale
+ * auteur: Sylvain Léchaire
+ * version: 0.1
+ * date de dernière modification 13.03.2020
+ */
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h>
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -17,37 +24,54 @@ do {
     scanf("%d", &menu);
     if (menu > 3)
         printf("erreur chiffre trop grand\n");
-
+    system("cls");
     switch (menu) {
         case 1:
+            system("cls");
             printf("demarrer le jeu\n");
             //afficher la grille
             /*/
-             * infos: 1= bateau présent 2: endroit touché mais raté 3= bateau touché 4=touché coulé
+             * infos: 0= case vide 1= bateau présent 2: endroit touché mais raté 3= bateau touché 4=touché coulé
              */
-            printf("  A  B  C  D  E  F  G  H  I  J  \n"
-                   "1 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "2 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "3 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "4 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "5 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "6 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "7 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "8 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "9 |-||-||-||-||-||-||-||-||-||-|\n"
-                   "10|-||-||-||-||-||-||-||-||-||-|\n");
-            int tableau [10][10];
+            int tableau [10] [10];
+            for (int j = 0; j <= 9 ; ++j) {
+                printf("\n");
+                for (int i = 0; i <= 9 ; ++i) {
+                    tableau [i] [j] = 1;
+                printf("0  ");
+                }
+
+            }
+            printf("\n");
+           // printf("  A  B  C  D  E  F  G  H  I  J  \n"
+                 //  "1 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"2 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"3 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"4 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"5 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"6 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"7 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"8 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"9 |-||-||-||-||-||-||-||-||-||-|\n"
+                   //"10|-||-||-||-||-||-||-||-||-||-|\n");
+
+
             //placer les bateaux
             //porte avions
-            for (int i = 0; i < 3 ; ++i) {
+            for (int i = 0; i < 4 ; ++i) {
                 tableau [0] [i] = 1;
                 printf("%d", tableau [0] [i]);
+            }
+            for (int k = 0; k <= 9 ; ++k) {
+                printf("%d \n", tableau [k] [k]);
+
             }
 
 
             break;
         case 2:
             do {
+                system("cls");
                 printf("aide\n");
                 printf("principe du jeux: vous possédez les navires suivants \n");
                 printf("1 portes avions qui prends 5 cases\n");
